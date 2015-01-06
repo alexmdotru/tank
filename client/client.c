43fed3b6c5f8654d56a73f2ef796907279310554
Vladislav Taravkov <vtaravkov@me.com>
2015-01-06 20:29:12 +0300

    load textures & set client flags

--- client/client.c
+++ client/client.c
@@ -1,29 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "client.h"
 #include "framework.h"
+#include "client.h"
+#include "graphics.h"
+
+void loadGame(client_t *client);
 
 int main(int argc, char **argv) {
-  client_t client;
-
   // Check command line
   if(argc != 3) {
     fprintf(stderr, "usage:\t%s host port", argv[0]);
-    exit(1);
+    exit(EXIT_FAILURE);
   }
 
   initFramework();
-  launchGame(client);
+  client_t client;
+  loadGame(&client);
 
   SDL_Delay(10000);
-  SDLNet_Quit();
-  SDL_Quit();
+  quitFramework();
   return 0;
 }
 
-void launchGame(client_t client) {
-  client.window = SDL_CreateWindow("TAHK-2015", SDL_WINDOWPOS_CENTERED,
+void loadGame(client_t *client) {
+  // Prepare renderer
+  client->window = SDL_CreateWindow("TAHK-2015", SDL_WINDOWPOS_CENTERED,
     SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
-  client.renderer = SDL_CreateRenderer(client.window, -1,
+  client->renderer = SDL_CreateRenderer(client->window, -1,
     SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  loadTextures(client);
+
+  // Set client flags
+  client->isPlaying = 0;
+  client->isInMainMenu = 1;
 }
