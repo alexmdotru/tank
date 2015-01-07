444c22698e8c2a4e3b8e3e1b5b10a00c71d1b3ec
Vladislav Taravkov <vtaravkov@me.com>
2015-01-07 16:51:36 +0300

    implement server side

--- server/server.c
+++ server/server.c
@@ -1,17 +1,71 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "framework.h"
+#include "network.h"
+#include "server.h"
+
+SDLNet_SocketSet createSocketSet(int size);
+void acceptConnections(server_t *server);
 
 int main(int argc, char **argv) {
   // Check command line
   if(argc != 2) {
-    fprintf(stderr, "usage:\t%s port", argv[0]);
-    exit(1);
+    fprintf(stderr, "usage:\t%s port\n", argv[0]);
+    exit(EXIT_FAILURE);
   }
 
+  // Init SDL2 frameworks
   initFramework();
 
-  SDLNet_Quit();
-  SDL_Quit();
+  server_t server;
+
+  // Set server host and port
+  server.shost = NULL;
+  server.sport = atoi(argv[1]);
+
+  // Wait for players to connect
+  acceptConnections(&server);
+
+  // Quit SDL2 frameworks
+  quitFramework();
   return 0;
+}
+
+SDLNet_SocketSet createSocketSet(int size) {
+  SDLNet_SocketSet socketSet;
+
+  socketSet = SDLNet_AllocSocketSet(size);
+  if(!socketSet) {
+    fprintf(stderr, "SDLNet_AllocSocketSet Error: %s\n", SDLNet_GetError());
+  }
+
+  return socketSet;
+}
+
+void acceptConnections(server_t *server) {
+  // No players
+  server->pCount = 0;
+
+  // Allocate socket set
+  server->socketSet = createSocketSet(2);
+
+  // Open server socket
+  server->socket = openTCPSocket(server->shost, server->sport);
+
+  fprintf(stderr, "Ready to accept connections!\n");
+
+  while(server->pCount < 2) {
+    TCPsocket pSocket;
+
+    pSocket = SDLNet_TCP_Accept(server->socket);
+    if(pSocket) {
+      fprintf(stderr, "Accepted from player %d.\n", server->pCount);
+      server->pSocket[server->pCount] = pSocket;
+      SDLNet_TCP_AddSocket(server->socketSet, pSocket);
+      server->pCount++;
+    }
+    else {
+      SDL_Delay(3000);
+    }
+  }
 }
