c355a7f604dc7c6c514c6503dc5edafa89832348
Vladislav Taravkov <vtaravkov@me.com>
2015-01-07 23:49:12 +0300

    remove function

--- common/network.c
+++ common/network.c
@@ -11,45 +11,9 @@ TCPsocket openTCPSocket(char *host, int port) {
   }
 
   socket = SDLNet_TCP_Open(&ip);
-
   if(!socket) {
     fprintf(stderr, "SDLNet_TCP_Open Error: %s\n", SDLNet_GetError());
   }
 
   return socket;
-}
-
-int connectToServer(void *data) {
-  int i;
-  client_t *client = (client_t*)data;
-
-  client->cstatus = CONNECTING;
-
-  for(i = 0; i < 5; i++) {
-    // Open socket
-    fprintf(stderr, "Connecting to %s:%d\n", client->shost, client->sport);
-    client->socket = openTCPSocket(client->shost, client->sport);
-
-    if(client->socket) {
-      client->cstatus = WAITING2ND;
-      SDLNet_TCP_Recv(client->socket, &client->id, 1);
-      fprintf(stderr, "Connection successful! Player id %d.\n", client->id);
-      SDLNet_TCP_Recv(client->socket, &client->id, 1);
-      client->cstatus = CONNECTED;
-      SDL_Delay(3000);
-      client->isInMainMenu = 0;
-      client->isPlaying = 1;
-      break;
-    }
-
-    fprintf(stderr, "Server didn't respond. Retrying...\n");
-    SDL_Delay(3000);
-  }
-
-  if(!client->socket) {
-    client->cstatus = CTIMEOUT;
-    return -1;
-  }
-
-  return 0;
 }
