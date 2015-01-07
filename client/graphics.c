638d0604b245aa0272a99eedfdc8743cec70260e
Vladislav Taravkov <vtaravkov@me.com>
2015-01-07 23:50:25 +0300

    implement texture loading

--- client/graphics.c
+++ client/graphics.c
@@ -12,6 +12,8 @@ SDL_Color COLOR_WHITE = { 255, 255, 255 };
 
 texture_t *loadLabel(char *text, int size, SDL_Color color, SDL_Renderer *renderer);
 void renderTexture(texture_t *texture, int x, int y, int sx, int sy, SDL_Renderer *renderer);
+SDL_Surface *pngToSurface(char *file);
+texture_t *loadTexture(SDL_Surface *src, int x, int y, int szx, int szy, SDL_Renderer *renderer);
 
 void loadFrame(client_t *client) {
   client->window = SDL_CreateWindow("TAHK-2015", SDL_WINDOWPOS_CENTERED,
@@ -25,11 +27,53 @@ void loadResources(client_t *client) {
   // Create and load labels
   client->logo1 = loadLabel("TAHK", 64, COLOR_RED, client->renderer);
   client->logo2 = loadLabel("2015", 64, COLOR_RED, client->renderer);
+
   // Connection labels
   client->lcing = loadLabel("соединяю...", 16, COLOR_WHITE, client->renderer);
   client->lcout = loadLabel("нет ответа", 16, COLOR_WHITE, client->renderer);
   client->lw2nd = loadLabel("жду второго игрока...", 16, COLOR_WHITE, client->renderer);
   client->lcned = loadLabel("игра начинается...", 16, COLOR_WHITE, client->renderer);
+
+  // Load textures.png into surface
+  client->textures = pngToSurface("../../resources/textures.png");
+
+  // Load water textures
+  client->water[0] = loadTexture(client->textures, 256, 80, 8, 8, client->renderer);
+  client->water[1] = loadTexture(client->textures, 264, 80, 8, 8, client->renderer);
+  client->water[2] = loadTexture(client->textures, 272, 80, 8, 8, client->renderer);
+}
+
+SDL_Surface *pngToSurface(char *file) {
+  SDL_Surface *surface;
+
+  surface = IMG_Load(file);
+  if(surface == NULL) {
+    fprintf(stderr, "IMG_Load Error: %s\n", IMG_GetError());
+  }
+
+  return surface;
+}
+
+texture_t *loadTexture(SDL_Surface *src, int x, int y, int szx, int szy, SDL_Renderer *renderer) {
+  texture_t *texture;
+  SDL_Surface *dst = SDL_CreateRGBSurface(0, szx, szy, 32, 0, 0, 0, 0);
+  texture = malloc(sizeof(texture_t));
+  texture->tex = NULL;
+
+  SDL_Rect rect = { x, y, szx, szy };
+  if(SDL_BlitSurface(src, &rect, dst, NULL) != 0) {
+    fprintf(stderr, "SDL_BlitSurface Error: %s\n", SDL_GetError());
+  }
+
+  texture->tex = SDL_CreateTextureFromSurface(renderer, dst);
+  if(texture->tex == NULL) {
+    fprintf(stderr, "SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
+  }
+
+  SDL_QueryTexture(texture->tex, NULL, NULL, &texture->width, &texture->height);
+
+  SDL_FreeSurface(dst);
+  return texture;
 }
 
 texture_t *loadLabel(char *text, int size, SDL_Color color, SDL_Renderer *renderer) {
@@ -86,7 +130,13 @@ void renderMenu(client_t *client) {
 }
 
 void render(client_t *client) {
+  // Clear render
+  SDL_RenderClear(client->renderer);
 
+  renderTexture(client->water[0], 112, 256, 1, 1, client->renderer);
+
+  // Update render
+  SDL_RenderPresent(client->renderer);
 }
 
 void renderTexture(texture_t *texture, int x, int y, int sx, int sy, SDL_Renderer *renderer) {
