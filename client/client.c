4b26b75a419ae1225b2c136d979d47f0ce178748
Vladislav Taravkov <vtaravkov@me.com>
2015-01-08 15:28:31 +0300

    implement map loader thread

--- client/client.c
+++ client/client.c
@@ -4,11 +4,15 @@
 #include "client.h"
 #include "graphics.h"
 #include "network.h"
+#include "map.h"
 
 void gameLoop(client_t *client);
+int connectToServerThread(void *data);
+int loadMapThread(void *data);
 void updateMenu(client_t *client);
+void renderMenu(client_t *client);
 void update(client_t *client);
-int connectToServer(void *data);
+void render(client_t *client);
 
 int main(int argc, char **argv) {
   // Check command line
@@ -23,17 +27,23 @@ int main(int argc, char **argv) {
   client_t client;
 
   // Set server host and port
-  client.shost = argv[1];
-  client.sport = atoi(argv[2]);
+  client.host = argv[1];
+  client.port = atoi(argv[2]);
 
   // Prepare renderer
-  loadFrame(&client);
+  client.graphics = malloc(sizeof(graphics_t));
+  loadFrame(client.graphics);
 
   // Load labels and textures
-  loadResources(&client);
+  loadResources(client.graphics);
 
   // Start game
   gameLoop(&client);
+
+  SDL_Delay(2000);
+
+  // Free renderer
+  freeFrame(client.graphics);
 
   // Quit SDL2 frameworks
   quitFramework();
@@ -43,10 +53,9 @@ int main(int argc, char **argv) {
 void gameLoop(client_t *client) {
   client->gameRunning = 1;
   client->isInMainMenu = 1;
-  client->waterAnim = 0;
 
-  SDL_Thread *cToServer;
-  cToServer = SDL_CreateThread(connectToServer, "cToServer", (void*)client);
+  SDL_Thread *cToServerT;
+  cToServerT = SDL_CreateThread(connectToServerThread, "cToServerT", (void*)client);
 
   while(client->gameRunning) {
     if(client->isPlaying) {
@@ -82,26 +91,29 @@ void update(client_t *client) {
   }
 }
 
-int connectToServer(void *data) {
+int connectToServerThread(void *data) {
   client_t *client = (client_t*)data;
 
   // Set connection status
   client->cstatus = CONNECTING;
 
   // Connection timeout
-  Uint32 timeout = SDL_GetTicks() + 30000;
-  fprintf(stderr, "Connecting to %s:%d\n", client->shost, client->sport);
+  int timeout = SDL_GetTicks() + 30000;
+  fprintf(stderr, "Connecting to %s:%d\n", client->host, client->port);
 
   while(SDL_GetTicks() < timeout) {
     // Open socket
-    client->socket = openTCPSocket(client->shost, client->sport);
+    client->socket = openTCPSocket(client->host, client->port);
 
     if(client->socket) {
-      SDLNet_TCP_Recv(client->socket, &client->id, 1);
-      fprintf(stderr, "Connection successful! Player id %d.\n", client->id);
+      SDLNet_TCP_Recv(client->socket, &client->playerID, 4);
+      fprintf(stderr, "Connection successful! Player ID %d.\n", client->playerID);
       client->cstatus = WAITING2ND;
       SDLNet_TCP_Recv(client->socket, &client->level, 4);
       client->cstatus = CONNECTED;
+      // Load map
+      SDL_Thread *loadMapT;
+      loadMapT = SDL_CreateThread(loadMapThread, "loadMapT", (void*)client);
       SDL_Delay(3000);
       client->isInMainMenu = 0;
       client->isPlaying = 1;
@@ -115,5 +127,17 @@ int connectToServer(void *data) {
     return 1;
   }
 
+  return 0;
+}
+
+int loadMapThread(void *data) {
+  client_t *client = (client_t*)data;
+  char  file[256];
+  char level[256];
+  sprintf(level, "%d", client->level);
+  strcpy(file, "resources/maps/level");
+  strcat(file, level);
+  strcat(file, ".map");
+  client->map = loadMap(file);
   return 0;
 }
