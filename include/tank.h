46f0937b5cdd2fd9aa3a76a69623cc144129476b
Vladislav Taravkov <vtaravkov@me.com>
2015-01-09 23:24:34 +0300

    define tank struct

---
+++ include/tank.h
@@ -0,0 +1,22 @@
+#pragma once
+
+typedef enum {
+  PLAYER,
+  ENEMY
+} driver_t;
+
+typedef enum {
+  UP = 0,
+  DOWN = 180,
+  LEFT = 270,
+  RIGHT = 90
+} direction_t;
+
+typedef struct {
+  driver_t driver;
+  direction_t direction;
+  int posX;
+  int posY;
+  int velocity;
+  int isMoving;
+} tank_t;
