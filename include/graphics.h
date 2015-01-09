7ef57926aa09a573adb5a92fc7346297fe097317
Vladislav Taravkov <vtaravkov@me.com>
2015-01-09 23:23:52 +0300

    tank texures

--- include/graphics.h
+++ include/graphics.h
@@ -13,10 +13,8 @@ typedef struct {
   // Main frame
   SDL_Window *window;
   SDL_Renderer *renderer;
-
   // Texture bank
   SDL_Surface *textures;
-
   // Labels
   texture_t *logo1;
   texture_t *logo2;
@@ -24,19 +22,20 @@ typedef struct {
   texture_t *lcout;
   texture_t *lw2nd;
   texture_t *lcned;
-
   // Brick
   texture_t *brick;
-
   // Steel
   texture_t *steel;
-
   // Water
   texture_t *water[3];
   int waterAnim;
+  // Tanks
+  texture_t *tank[2][2];
+  int tankAnim[2];
+  // Map viewport
+  SDL_Rect mapView;
 } graphics_t;
 
 void loadFrame(graphics_t *graphics);
 void freeFrame(graphics_t *graphics);
-void loadResources(graphics_t *graphics);
 void freeResources(graphics_t *graphics);
