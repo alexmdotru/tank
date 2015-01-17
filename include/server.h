a3ffeaeb7029c5e88014712e13c58b1f6ac451ab
Vladislav Taravkov <vtaravkov@me.com>
2015-01-18 01:45:12 +0300

    enemy tanks & map

--- include/server.h
+++ include/server.h
@@ -1,21 +1,28 @@
 #pragma once
 
 #include "framework.h"
+#include "tank.h"
+#include "map.h"
+
+#define ENEMIES 4
 
 typedef struct {
   // Server host and port
   char *host;
   int port;
-
   // Level to play
   int level;
-
   // Server and client sockets
   TCPsocket sSocket;
   TCPsocket cSocket[2];
-
   int serverRunning;
-
   // Current player id
   int playerID;
+  // Enemies
+  uint8_t enemies;
+  uint32_t enemiesDelay;
+  // Tanks
+  tank_t tank[6];
+  // Map
+  map_t *map;
 } server_t;
