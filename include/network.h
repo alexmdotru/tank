898c88962107be7a68168025f16a67cf9639ebca
Vladislav Taravkov <vtaravkov@me.com>
2015-01-18 01:44:41 +0300

    send & recv tank struct

--- include/network.h
+++ include/network.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#include "tank.h"
+
 TCPsocket openTCPSocket(char *host, int port);
+void sendTankStruct(tank_t *tank, TCPsocket socket);
+void recvTankStruct(tank_t *tank, TCPsocket socket);
