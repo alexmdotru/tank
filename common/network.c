31def347367d4805c0ee156ac1be7b5a07e3f003
Vladislav Taravkov <vtaravkov@me.com>
2015-01-24 22:43:21 +0300

    send full tank struct over tcp

--- common/network.c
+++ common/network.c
@@ -31,20 +31,24 @@ void sendTankStruct(tank_t *tank, TCPsocket socket) {
   SDLNet_TCP_Send(socket, &null, 1);
   if(null) return;
 
-  // SDLNet_TCP_Send(&socket, &tank->driver, sizeof(driver_t));
+  SDLNet_TCP_Send(socket, &tank->driver, sizeof(driver_t));
   SDLNet_TCP_Send(socket, &tank->direction, sizeof(direction_t));
   SDLNet_TCP_Send(socket, &tank->posX, 4);
   SDLNet_TCP_Send(socket, &tank->posY, 4);
-  // SDLNet_TCP_Send(&socket, &tank->velocity, 1);
   SDLNet_TCP_Send(socket, &tank->isMoving, 1);
+  SDLNet_TCP_Send(socket, &tank->isOnTheWay, 1);
+  SDLNet_TCP_Send(socket, &tank->moveDelay, 4);
   SDLNet_TCP_Send(socket, &tank->isFiring, 1);
-  // SDLNet_TCP_Send(&socket, &tank->isOnTheWay, 1);
-  // SDLNet_TCP_Send(&socket, &tank->moveDelay, 4);
+  SDLNet_TCP_Send(socket, &tank->null, 1);
+  SDLNet_TCP_Send(socket, &tank->destrBlock, 2);
+  SDLNet_TCP_Send(socket, &tank->id, 2);
+  SDLNet_TCP_Send(socket, &tank->winsTheGame, 1);
   SDLNet_TCP_Send(socket, &tank->fire.posX, 4);
   SDLNet_TCP_Send(socket, &tank->fire.posY, 4);
-  // SDLNet_TCP_Send(socket, &tank->fire.explodes, 1);
+  SDLNet_TCP_Send(socket, &tank->fire.explodes, 1);
   SDLNet_TCP_Send(socket, &tank->fire.direction, sizeof(direction_t));
-  SDLNet_TCP_Send(socket, &tank->destrBlock, 2);
+  SDLNet_TCP_Send(socket, &tank->block[0], 1);
+  SDLNet_TCP_Send(socket, &tank->block[1], 1);
 }
 
 void recvTankStruct(tank_t *tank, TCPsocket socket) {
@@ -59,18 +63,22 @@ void recvTankStruct(tank_t *tank, TCPsocket socket) {
   }
 
   tank->null = 0;
-  // SDLNet_TCP_Send(&socket, &tank->driver, sizeof(driver_t));
+  SDLNet_TCP_Recv(socket, &tank->driver, sizeof(driver_t));
   SDLNet_TCP_Recv(socket, &tank->direction, sizeof(direction_t));
   SDLNet_TCP_Recv(socket, &tank->posX, 4);
   SDLNet_TCP_Recv(socket, &tank->posY, 4);
-  // SDLNet_TCP_Send(&socket, &tank->velocity, 1);
   SDLNet_TCP_Recv(socket, &tank->isMoving, 1);
+  SDLNet_TCP_Recv(socket, &tank->isOnTheWay, 1);
+  SDLNet_TCP_Recv(socket, &tank->moveDelay, 4);
   SDLNet_TCP_Recv(socket, &tank->isFiring, 1);
-  // SDLNet_TCP_Send(&socket, &tank->isOnTheWay, 1);
-  // SDLNet_TCP_Send(&socket, &tank->moveDelay, 4);
+  SDLNet_TCP_Recv(socket, &tank->null, 1);
+  SDLNet_TCP_Recv(socket, &tank->destrBlock, 2);
+  SDLNet_TCP_Recv(socket, &tank->id, 2);
+  SDLNet_TCP_Recv(socket, &tank->winsTheGame, 1);
   SDLNet_TCP_Recv(socket, &tank->fire.posX, 4);
   SDLNet_TCP_Recv(socket, &tank->fire.posY, 4);
-  // SDLNet_TCP_Recv(socket, &tank->fire.explodes, 1);
+  SDLNet_TCP_Recv(socket, &tank->fire.explodes, 1);
   SDLNet_TCP_Recv(socket, &tank->fire.direction, sizeof(direction_t));
-  SDLNet_TCP_Recv(socket, &tank->destrBlock, 2);
+  SDLNet_TCP_Recv(socket, &tank->block[0], 1);
+  SDLNet_TCP_Recv(socket, &tank->block[1], 1);
 }
